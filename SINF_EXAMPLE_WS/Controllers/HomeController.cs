e4091d523016c81f552c13ae851c58b2d8130af0
Ricardo Loureiro <ricardojrloureiro@gmail.com>
2015-10-16 17:20:00 +0100

    Initial project commit with index view

---
+++ SINF_EXAMPLE_WS/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SINF_EXAMPLE_WS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SINF_EXAMPLE_WS/Models/IntegrationPri.cs
@@ -0,0 +1,48 @@
+﻿using Interop.StdBE800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SINF_EXAMPLE_WS.Models
+{
+    public class IntegrationPri
+    {
+
+
+        public static List<Cliente> ListaClientes()
+        {
+
+
+            StdBELista objList;
+
+            List<Cliente> listClientes = new List<Cliente>();
+
+            if (PriEngine.InitializeCompany(SINF_EXAMPLE_WS.Properties.Settings.Default.Company.Trim(), SINF_EXAMPLE_WS.Properties.Settings.Default.User.Trim(), SINF_EXAMPLE_WS.Properties.Settings.Default.Password.Trim()) == true)
+            {
+
+                objList = PriEngine.Engine.Consulta("SELECT Cliente, Nome, Moeda, NumContrib as NumContribuinte, Fac_Mor AS campo_exemplo FROM  CLIENTES");
+
+                while (!objList.NoFim())
+                {
+                    listClientes.Add(new Cliente
+                    {
+                        CodCliente = objList.Valor("Cliente"),
+                        NomeCliente = objList.Valor("Nome"),
+                        Moeda = objList.Valor("Moeda"),
+                        NumContribuinte = objList.Valor("NumContribuinte"),
+                        Morada = objList.Valor("campo_exemplo")
+                    });
+                    objList.Seguinte();
+
+                }
+
+                return listClientes;
+            }
+            else
+                return null;
+        }
+    
+    
+    }
+}
