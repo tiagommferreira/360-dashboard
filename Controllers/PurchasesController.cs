637feb81cfdffa60af8194b76308031d2558fdc3
Ricardo Loureiro <ricardojrloureiro@gmail.com>
2015-12-11 17:45:30 +0000

    Purchases

---
+++ Controllers/PurchasesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using SINF_EXAMPLE_WS.Models;
+
+
+namespace SINF_EXAMPLE_WS.Controllers
+{
+    public class PurchasesController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync("http://localhost:49990/api/Compras");
+            var compras = await response.Content.ReadAsAsync<IEnumerable<Object>>();
+
+            ViewBag.Compras = compras;
+
+            return View(compras);
+        }
+
+        public async Task<ActionResult> View(string serie, string tipoDoc, string numDoc)
+        {
+            System.Diagnostics.Debug.WriteLine(serie + " " + tipoDoc + " " + numDoc);
+            var client = new HttpClient();
+            var response = await client.GetAsync("http://localhost:49990/api/Compras/Documento?" + "serie=" + serie + "&tipoDoc=" + tipoDoc + "&numDoc=" + numDoc);
+
+            var compras = await response.Content.ReadAsAsync<IEnumerable<Object>>();
+
+            ViewBag.compras = compras;
+            return View(compras);
+        }
+    }
+}
